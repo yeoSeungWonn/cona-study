c2b7b500756718f9d57191b6eacee4472558c789
dutmddnjs <dutmddnjs@hanmail.net>
2022-12-01 22:24:13 +0900

    Implement #4 - MemberRepo
    Member의 레포지토리를 jpa repository인터페이스를 이용해 구현

---
+++ study/src/main/java/cona/study/domain/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package cona.study.domain.member.repository;
+
+import cona.study.domain.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findAll();
+
+    Optional<Member> findById(Long id);
+}

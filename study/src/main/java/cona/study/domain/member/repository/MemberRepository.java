a8e5c730a2c42f7f454782397b029448c1805284
dutmddnjs <dutmddnjs@hanmail.net>
2022-12-04 20:36:38 +0900

    update - 리포지토리
    쓰지 않는 리포지토리 상속 제거

--- study/src/main/java/cona/study/domain/member/repository/MemberRepository.java
+++ study/src/main/java/cona/study/domain/member/repository/MemberRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends
-        JpaRepository<Member, Long>,
-        MemberRepositoryCustom {
+        JpaRepository<Member, Long> {
 
     Member save(Member member);
 
